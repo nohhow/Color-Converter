972ccdb016fae124362e6bc560ff3bab2e16e419
nohhow <xksrma97@gmail.com>
2021-11-04 05:43:42 +0900

    target update

--- javascript/index.js
+++ javascript/index.js
@@ -12,29 +12,35 @@ function goConvert(){
   var target = codeForm.elements[0].value;
 
   // 만약 타겟이 HEX TYPE이라면
+  if(target.substr(0,1) == "#"){
+    // hex 코드에서 #제거
+    var hex = target.trim().replace( "#", "" );
 
-  // hex 코드에서 #제거
-  var hex = target.trim().replace( "#", "" );
+    /* rgb로 각각 분리해서 배열에 담기. */
+    var rgb = ( 3 === hex.length ) ?
+    hex.match( /[a-f\d]/gi ) : hex.match( /[a-f\d]{2}/gi );
 
-  /* rgb로 각각 분리해서 배열에 담기. */
-  var rgb = ( 3 === hex.length ) ?
-  hex.match( /[a-f\d]/gi ) : hex.match( /[a-f\d]{2}/gi );
+    // Input이 #FFFFFF 일 때, 현재 까지 rgb = [FF,FF,FF] 이 상태..   forEach 통해서 loop
+    if(rgb != null){
+      rgb.forEach(function (str, index, array){
+          /* rgb 각각의 헥사값이 한자리일 경우, 두자리로 변경하기. */
+          if ( str.length == 1 ) str = str + str;
 
-  // Input이 #FFFFFF 일 때, 현재 까지 rgb = [FF,FF,FF] 이 상태..   forEach 통해서 loop
-  if(rgb != null){
-    rgb.forEach(function (str, index, array){
-        /* rgb 각각의 헥사값이 한자리일 경우, 두자리로 변경하기. */
-        if ( str.length == 1 ) str = str + str;
+          /* 10진수로 변환하기. */
+          array[ index ] = parseInt( str, 16 );
+      });
+    }else{
+      alert('값을 확인하세요.');
+      return;
+    }
 
-        /* 10진수로 변환하기. */
-        array[ index ] = parseInt( str, 16 );
-    });
-  }else{
-    alert('값을 확인하세요.');
-    return;
+    // 결과 표시
+    conversionResult.innerText="rgb(" + rgb.join(", ") + ")";
+  }
+  else{
+    alert("something wrong");
   }
 
-  // 결과 표시
-  conversionResult.innerText="rgb(" + rgb.join(", ") + ")";
+
 
 }
