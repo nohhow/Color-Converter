33fbdabc8b7860330d230fe81565f692ec053678
nohhow <xksrma97@gmail.com>
2021-11-07 13:20:57 +0900

    hex to rgb update

--- javascript/index.js
+++ javascript/index.js
@@ -38,8 +38,28 @@ function goConvert(){
     conversionResult.innerText="rgb(" + rgb.join(", ") + ")";
   }
   else{
-    alert("something wrong");
+    /*
+    ** 컬러값과 쉼표만 남기고 삭제하기.
+    ** 쉼표(,)를 기준으로 분리해서, 배열에 담기.
+    */
+    var rgb = target.replace( /[^%,.\d]/g, "" ).split( "," );
+
+    rgb.forEach(function (str, x, arr){
+
+        /* 컬러값이 "%"일 경우, 변환하기. */
+        if ( str.indexOf( "%" ) > -1 ) str = Math.round( parseFloat(str) * 2.55 );
+
+        /* 16진수 문자로 변환하기. */
+        str = parseInt( str, 10 ).toString( 16 );
+        if ( str.length === 1 ) str = "0" + str;
+
+        arr[ x ] = str;
+    });
+
+    conversionResult.innerText= "#" + rgb.join( "" );
+
   }
+
 
 
 
